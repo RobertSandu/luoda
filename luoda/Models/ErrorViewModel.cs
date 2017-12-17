01e792b2619db56330611dfaf676744959edaa7b
Robert Sandu <robert-stelian.sandu@my.fmi.unibuc.ro>
2017-12-17 19:40:20 +0200

    initial commit

---
+++ luoda/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace luoda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ luoda/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace luoda.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
